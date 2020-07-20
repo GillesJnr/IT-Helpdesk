70f7ec1008704af2100beaaa4854774680a2ee4f
Gabriel Gilles <gabbygilles1@gmail.com>
2020-07-20 11:37:25 +0000

    corrected app registration in settings

--- Helpdesk/Helpdesk/settings.py
+++ Helpdesk/Helpdesk/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.app.AppConfig',
-    'crispy-forms',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
